135b05a7eb0a8debb1883190da312b22f6a37981
farce-codec <fabianarce161@gmail.com>
2022-06-08 11:18:39 -0600

    Semana 3
    Semana 3

---
+++ Semana3/Semana3/src/semana3/Semana3.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fabia
+ */
+public class Semana3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Ciclo FOR //
+//        for (int i = 0; i<5; i++){
+//            System.out.println(i);
+//        }
+        
+        // Ciclo WHILE //
+//        int i = 0;
+//        
+//        while (i<5){
+//            System.out.println(i);
+//            i++;
+//        }
+//        
+//        int j = 0;        
+//        boolean flag = true;
+//        
+//        while (flag){
+//            if (j<5){
+//                System.out.println(j);
+//                j++;
+//            }else{
+//                flag = false;
+//            }
+//        }
+        
+        // EJEMPLO #1 //
+        
+//        for (int m = 0; m<5; m++){
+//            System.out.println("@@@@@@@@@@");
+//        }
+        
+//        int i = 0;
+//        boolean flag = true;
+//        
+//        while (flag){
+//            System.out.println("@@@@@@@@@@");
+//            i++;
+//            if (i==5){
+//                flag = false;
+//            }
+//        }
+        
+        // Ciclo DO WHILE //
+//        int j = 0;
+//        
+//        do{
+//            System.out.println(j);
+//            j++;
+//        }
+//        while (j<5);
+        
+        // EJEMPLO #2 //
+        
+//       for (int i = 0; i<5; i++){
+//           for (int j = 0; j<10; j++){
+//               System.out.print("@");
+//           }
+//           System.out.println(" ");
+//       }
+       
+       // EJEMPLO #3 //
+       
+//       int num = Integer.parseInt(JOptionPane.showInputDialog("Por favor "
+//               + "digite un número"));
+//       
+//       for (int i = 1; i<11; i++){
+//           System.out.println(num+" x "+i+" = "+num*i);
+//       } 
+       
+       // EJEMPLO #4 //
+       
+//       int index = Integer.parseInt(JOptionPane.showInputDialog("Por favor "
+//               + "digite un número"));
+//               
+//       for (int i = 0; i<index; i++){
+//           for (int j = 0; j<i+1; j++){
+//               System.out.print("*");
+//           }
+//           System.out.println(" ");
+//       }
+       
+    }
+}
